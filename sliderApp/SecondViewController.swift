a3208a293005e1ffdda26427aecc0a53f3cacaf3
Vlad <vlad14196@gmail.com>
2022-01-02 12:34:15 +0300

    code 2

---
+++ sliderApp/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  sliderApp
+//
+//  Created by user on 26.12.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
--- sliderApp/ViewController.swift
+++ sliderApp/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        setColor()
+        setValue(for: redLabel. greenLabel,blueLabel)
+        
     }
-
-
+    private func setColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    @IBAction func rgbSlider( sender: UISlider) {
+        setColor()
+        switch sender {
+        case redSlider:
+        }
+    }
+    func setValue(for Labels: UILabel...) {
+        Labels.forEach { label in
+            switch label {
+            case redLabel: label.text = string(from: redSlider)
+            }
+        }
+    }
+    private func string() {
+        
+    }
 }
 
